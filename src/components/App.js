a3d2140ec6d52c08443694a4e260ae2cc0564d40
deepakyadav121999 <dy44444221@gmail.com>
2023-01-21 22:04:03 +0530

    gg

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,12 @@ import Footer from "./Footer";
 function App() {
     return (
         <div className="container">
-            {/* <header id="my-header">This is the header</header>
+            <header id="my-header">This is the header</header>
             <main id="my-main">This is the main</main>
-            <footer id="my-footer">This is the footer</footer> */}
-            <Header id="header" content="This is the header"/>
+            <footer id="my-footer">This is the footer</footer>
+            {/* <Header id="header" content="This is the header"/>
             <Main id="main" content="This is the main"/>
-            <Footer id="footer"  content="This is the footer"/>
+            <Footer id="footer"  content="This is the footer"/> */}
 
             
         </div>
